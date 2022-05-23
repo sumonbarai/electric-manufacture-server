6b819cf35ff0a8a57768d210b240e4c2a68043ce
sumonbarai <sumonbarai78@gmail.com>
2022-05-23 22:06:47 +0600

    server code start

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
+});
